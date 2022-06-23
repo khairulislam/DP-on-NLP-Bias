6104b403d7cddfb6ab4d9762e84b781e993deffa
Md. Khairul Islam <khairulislamtanim@gmail.com>
2022-06-23 00:05:50 -0400

    Add background prediction based metrics (#24)

--- src/ucberkeley/batch-benchmark.py
+++ src/ucberkeley/batch-benchmark.py
@@ -15,52 +15,27 @@ dataset_name = 'ucberkeley'
 model_name = 'bert-base-uncased'
 raw_id_column = 'comment_id'
 dataset_directory = f'../../results/{dataset_name}/'
+dataset_directory = '../../../experiment/ucberkeley 1e-3/'
 epsilon_list = [0.5, 1.0, 3.0, 6.0, 9.0]
 
 group_map = {
-    'men vs women': {
-        'unprivileged':['target_gender_women'],
-        'privileged':['target_gender_men']
-    },
-    'men vs transgender': {
-        'unprivileged':['target_gender_transgender'],
-        'privileged':['target_gender_men']
-    },
-    'white vs black': {
-        'unprivileged':['target_race_black'],
-        'privileged': ['target_race_white']
-    },
-    'white vs asian': {
-        'unprivileged':['target_race_asian'],
-        'privileged': ['target_race_white']
-    }
-    # 'white vs latino': {
-    #     'unprivileged':['target_race_latinx'],
-    #     'privileged': ['target_race_white']
-    # },
-    # 'christian vs jewish': {
-    #     'unprivileged':['target_religion_jewish'],
-    #     'privileged': ['target_religion_christian']
-    # },
-    # 'christian vs muslim': {
-    #     'unprivileged':['target_religion_muslim'],
-    #     'privileged': ['target_religion_christian']
-    # }
+    'gender':['target_gender_men', 'target_gender_women', 'target_gender_transgender'],
+    'race':['target_race_white','target_race_black', 'target_race_asian']
 }
 
-identities = []
-for group_key in group_map.keys():
-    subgroup_map = group_map[group_key]
-    for subgroup_key in subgroup_map.keys():
-        identities.extend(subgroup_map[subgroup_key])
+attributes = list(group_map.keys())
+protected_subgroups = []
+for attribute in group_map.keys():
+    subgroups = group_map[attribute]
+    protected_subgroups.extend(subgroups)
 
-print(f'Target identities {list(set(identities))}')
-print(f'Target groups {list(group_map.keys())}')
+print(f'Sensitive attributes {attributes}')
+print(f'Protected subgroups {protected_subgroups}')
 
 counts = []
 for run in range(1, 4):
     run_folder = f'{dataset_directory}/run {run}'
-    model_folder = os.path.join(run_folder, model_name)
+    model_folder = run_folder # os.path.join(run_folder, model_name)
     normal_folder = os.path.join(model_folder, 'normal')
     result_filepath = os.path.join(normal_folder, 'results.csv')
 
@@ -81,12 +56,12 @@ for run in range(1, 4):
 
     result = result.merge(test_df.drop(columns=extra_columns), on=id_column, how='inner').reset_index(drop=True)
     result[prediction_column] = result[probability_column] >=0.5
-    result = binarize(result, [target_column] + identities)
+    result = binarize(result, [target_column] + protected_subgroups)
 
-    bias_results = get_all_bias(group_map, result)
+    bias_results = get_all_bias(result, protected_subgroups)
     bias_results.round(3).to_csv(os.path.join(normal_folder, 'bias.csv'), index=False)
 
-    overall_results = get_overall_results(group_map, result)
+    overall_results = get_overall_results(result, protected_subgroups)
     overall_results.round(3).to_csv(os.path.join(normal_folder, 'overall_results.csv'), index=False)
 
     for epsilon in epsilon_list:
@@ -100,20 +75,20 @@ for run in range(1, 4):
         dp_result = dp_result.merge(test_df.drop(columns=extra_columns), on=id_column, how='inner').reset_index(drop=True)
         
         dp_result[prediction_column] = dp_result[probability_column] >=0.5
-        dp_result = binarize(dp_result, [target_column] + identities)
+        dp_result = binarize(dp_result, [target_column] + protected_subgroups)
 
-        bias_results = get_all_bias(group_map, dp_result)
+        bias_results = get_all_bias(dp_result, protected_subgroups)
         bias_results.round(3).to_csv(os.path.join(dp_folder, 'bias.csv'), index=False)
 
-        overall_results = get_overall_results(group_map, dp_result)
+        overall_results = get_overall_results(dp_result, protected_subgroups)
         overall_results.round(3).to_csv(os.path.join(dp_folder, 'overall_results.csv'), index=False)
 
     # calculate identity count for all classes
-    train_df = pd.read_csv(os.path.join(run_folder, 'train.csv'))
-    count_df = get_identity_count(train_df, test_df, identities)
-    count_df.to_csv(os.path.join(run_folder, 'count.csv'), index=False)
-    counts.append(count_df)
+#     train_df = pd.read_csv(os.path.join(run_folder, 'train.csv'))
+#     count_df = get_identity_count(train_df, test_df, protected_subgroups)
+#     count_df.to_csv(os.path.join(run_folder, 'count.csv'), index=False)
+#     counts.append(count_df)
 
 
-count_df = pd.concat(counts).groupby('Identity').agg('mean').round().reset_index()
-count_df.to_csv(os.path.join(dataset_directory, 'count.csv'), index=False)
+# count_df = pd.concat(counts).groupby('Identity').agg('mean').round().reset_index()
+# count_df.to_csv(os.path.join(dataset_directory, 'count.csv'), index=False)
